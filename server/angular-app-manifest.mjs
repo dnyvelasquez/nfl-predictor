c8aef05d3b132bf7b210a0007a252a62a4bc232e
dnyvelasquez <96837442+dnyvelasquez@users.noreply.github.com>
2025-09-11 23:29:10 +0000

    Deploying to gh-pages from @ dnyvelasquez/nfl-predictor@6e5cbaa97ab880d6aa4970197d71a7833a238967 🚀

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -24,8 +24,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 69380, hash: '3328adacdad267acc67586f78b0b06ff761707507e8eba50f283c3aae238dd01', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17145, hash: 'ff56302f538f5b4e18eefbc387f45bab03b37d6c8cecff589f201104f35a809d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 69380, hash: '60968e2e8946e80c2d2f4a9e0d334f68d9391ece40541fe959049879b60eff7b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17145, hash: 'd8125977519cdb657b7bfb2e226ffb0f497ac789dd428574834d9311f59b76d8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'index.html': {size: 74926, hash: '18c628141e7085cfec6bf452da6f95fe28984e9a1e293763c7a1b8f6aa25a6de', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'reglamento/index.html': {size: 77198, hash: 'a2a0170c27072fddbdb913dc60e5a7bf0df23d8f0d2a33ce4c2d2ce7ba4fa3a6', text: () => import('./assets-chunks/reglamento_index_html.mjs').then(m => m.default)},
     'equipos/index.html': {size: 86387, hash: 'c265a53a4b5b4772f9629f320f2d0fc0fd9704df5bac372b566efa5eeaf40fef', text: () => import('./assets-chunks/equipos_index_html.mjs').then(m => m.default)},
