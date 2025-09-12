ed8115ac36cb2834ed6aa5d79f8eaa796804bd72
dnyvelasquez <96837442+dnyvelasquez@users.noreply.github.com>
2025-09-12 03:35:42 +0000

    Deploying to gh-pages from @ dnyvelasquez/nfl-predictor@033ed9f04efb23b8a302f69f5c8ac8fa967f99b7 🚀

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -24,12 +24,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 69380, hash: '60968e2e8946e80c2d2f4a9e0d334f68d9391ece40541fe959049879b60eff7b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17145, hash: 'd8125977519cdb657b7bfb2e226ffb0f497ac789dd428574834d9311f59b76d8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'equipos/index.html': {size: 86387, hash: 'c265a53a4b5b4772f9629f320f2d0fc0fd9704df5bac372b566efa5eeaf40fef', text: () => import('./assets-chunks/equipos_index_html.mjs').then(m => m.default)},
-    'reglamento/index.html': {size: 77198, hash: 'a2a0170c27072fddbdb913dc60e5a7bf0df23d8f0d2a33ce4c2d2ce7ba4fa3a6', text: () => import('./assets-chunks/reglamento_index_html.mjs').then(m => m.default)},
-    'puntajes/index.html': {size: 83742, hash: 'be587c97b08841fbdc22fe50ccd37fb1b83fd6b080b0ed8e51a7d020ac365387', text: () => import('./assets-chunks/puntajes_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 74926, hash: '18c628141e7085cfec6bf452da6f95fe28984e9a1e293763c7a1b8f6aa25a6de', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 69380, hash: '6791381e29f2c4a27cf6173cefdc4251d536d42a26e5026a0c7aabc5ea11d643', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17145, hash: '2d497b2c5759896e74d4818d9b543ba1497bd56904cd71364c64df4acbf664df', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 74927, hash: '2552297059a2889df307e20c93fd9eae1cca97eb8300b8768e4ba753a18ed58c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'equipos/index.html': {size: 86388, hash: '150391d1ccf5fc1c039060bfe67809746154ce6b35cf28319895d6e5ee1de1b5', text: () => import('./assets-chunks/equipos_index_html.mjs').then(m => m.default)},
+    'reglamento/index.html': {size: 77198, hash: '5463a6b85e2aea2f17950f76ec0078377a06df4e3b57b98e22282a0a266ed4e0', text: () => import('./assets-chunks/reglamento_index_html.mjs').then(m => m.default)},
+    'puntajes/index.html': {size: 83744, hash: '09ece67f2b259f404400b18c3d00c7288c0a8e8db59901ea69ae7daf47a04a8e', text: () => import('./assets-chunks/puntajes_index_html.mjs').then(m => m.default)},
     'styles-BSXEOMAR.css': {size: 53687, hash: '5LJiXKB3p/k', text: () => import('./assets-chunks/styles-BSXEOMAR_css.mjs').then(m => m.default)}
   },
 };
