432abd372f01d73a5600e14662977d2ffc59966a
dnyvelasquez <dny.velasquez@gmail.com>
2025-09-12 21:52:38 -0500

    juegos menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './components/home/home';
 import { Equipos } from './components/equipos/equipos';
 import { TablaPuntajes } from './components/tabla-puntajes/tabla-puntajes';
 import { Reglamento } from './components/reglamento/reglamento';
+import { Juegos } from './components/juegos/juegos';
 
 export const routes: Routes = [
   { path: '', component: Home }, 
-  { path: 'equipos', component: Equipos },
   { path: 'puntajes', component: TablaPuntajes },
+  { path: 'equipos', component: Equipos },
+  { path: 'juegos', component: Juegos },
   { path: 'reglamento', component: Reglamento }
 ];
---
+++ src/app/components/juegos/juegos.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-juegos',
+  standalone: true,
+  imports: [],
+  templateUrl: './juegos.html',
+  styleUrl: './juegos.css'
+})
+export class Juegos {
+
+}
