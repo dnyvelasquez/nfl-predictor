4922d323e931b8c164699ba7652c020f8316b400
dnyvelasquez <dny.velasquez@gmail.com>
2025-09-16 17:42:35 -0500

    conferences colors

--- src/app/components/tabla-puntajes/tabla-puntajes.ts
+++ src/app/components/tabla-puntajes/tabla-puntajes.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { Service, Participante, Equipo } from '../../services/data';
+import { Service, Participante } from '../../services/data';
 import { AsyncPipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatChipsModule } from '@angular/material/chips';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabla-puntajes',
@@ -15,8 +16,9 @@ import { Observable } from 'rxjs';
     MatDividerModule,
     MatTableModule,
     MatChipsModule,
-    AsyncPipe
-  ],
+    AsyncPipe,
+    CommonModule
+],
   templateUrl: './tabla-puntajes.html',
   styleUrls: ['./tabla-puntajes.css']
 })
@@ -30,6 +32,10 @@ export class TablaPuntajes  {
     this.participantes$ = this.service.getParticipantesConPuntaje();
     
   }
+
+  getDivisiones(participante: any): string[] {
+    return participante.equipos?.map((eq: any) => eq.division) ?? [];
+}
 
 }
 
