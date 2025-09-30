a072653dbd5288780f1aff6ac3d7bf6354e006d3
dnyvelasquez <dnyvelasquez@users.noreply.github.com>
2025-09-30 04:22:05 +0000

    deploy: 87dcaffee9bb1dab1a7e731e00d5e78a6ecdb3ab

---
+++ chunk-CEDPZFL3.js
@@ -0,0 +1,1 @@
+import{e}from"./chunk-VOSPIT4N.js";var p=e((s,t)=>{t.exports=XMLHttpRequest});export default p();
---
+++ chunk-MAQGHWLI.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e,g as f}from"./chunk-65CJINC2.js";import"./chunk-VOSPIT4N.js";f();export{c as Headers,d as Request,e as Response,b as default,a as fetch};
